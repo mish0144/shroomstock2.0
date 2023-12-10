f6249f11fa9699d24c1b247b38a8b234cdb26e59
Kiameg <kiax1262@stud.kea.dk>
2023-12-10 17:20:50 +0100

    program

--- src/components/Program.jsx
+++ src/components/Program.jsx
@@ -1,15 +1,21 @@
 // Importér useEffect og useState fra React samt den tilhørende CSS-fil.
 import { useEffect, useState } from "react";
 import "../css/program.css";
+import blueScene from "../img/blue_flower.svg";
+import pinkScene from "../img/pink_flower.svg";
+import yellowScene from "../img/yellow_flower.svg";
 
 // Definér din funktionelle komponent "Index".
 function Index() {
   // Opret to state-variabler, "data" og "times", ved hjælp af useState-hooket.
   const [data, setData] = useState([]);
   const [times, setTimes] = useState([]);
+  const [isClicked, setIsClicked] = useState(false);
 
   // Funktion til at håndtere klik på ugedagsknapper.
   function handleClick(day) {
+    setTimes([]);
+    setIsClicked(true);
     // Hent data fra "http://localhost:8080/schedule" via en GET-anmodning.
     fetch("http://localhost:8080/schedule", {
       method: "GET",
@@ -17,17 +23,17 @@ function Index() {
       .then(function (response) {
         return response.json();
       })
-      .then(function (times) {
+      .then(function (schedule) {
         // Konverter objektet "times" til et array af værdier.
-        times = Object.values(times);
+        schedule = Object.values(schedule);
         // Iterér over arrayet og opdater "times"-staten baseret på den valgte dag.
-        times.forEach((stage) => {
-          setTimes(stage[day]);
+        schedule.forEach((stage, index) => {
+          const timesWithScene = stage[day].map((timeSlot) => ({ ...timeSlot, scene: index }));
+          setTimes((prevTimes) => [...prevTimes, ...timesWithScene]);
         });
       })
       .catch((err) => console.error(err));
   }
-
   // Funktion til at hente band-data ved komponentens montage.
   function bandsFetch() {
     // Hent data fra "http://localhost:8080/bands" via en GET-anmodning.
@@ -43,7 +49,15 @@ function Index() {
       })
       .catch((err) => console.error(err));
   }
+  function imageBand(band) {
+    const foundBand = data.find((obj) => obj.name === band);
 
+    if (foundBand) {
+      return foundBand.logo;
+    } else {
+      return "";
+    }
+  }
   // Anvend useEffect-hooket til at køre "bandsFetch" ved komponentens montage.
   useEffect(() => {
     bandsFetch();
@@ -66,21 +80,43 @@ function Index() {
       {/* Sektion med band-information */}
       <section className="bands">
         {/* Map over hentede band-data og generér artikler for hvert band */}
-        {data.map((band) => (
-          // eslint-disable-next-line react/jsx-key (advarsel: husk at tilføje en unik nøgle til hvert element)
-          <article>
-            {/* Sektion med bandnavn og logo */}
-            <section className="band">
-              <h2>{band.name}</h2>
-              <img src={band.logo.startsWith("https") ? band.logo : "http://localhost:8080/logos/" + band.logo} alt="band image" />
-            </section>
-
-            {/* Sektion med tidsinformation */}
-            <section className="time">
-              <p>10:00</p>
-            </section>
-          </article>
-        ))}
+        {isClicked
+          ? times.map((time, index) => {
+              let imgString = imageBand(time.act);
+              let sceneImgSrc;
+              if (time.scene === 0) {
+                sceneImgSrc = pinkScene;
+              } else if (time.scene === 1) {
+                sceneImgSrc = yellowScene;
+              } else if (time.scene === 2) {
+                sceneImgSrc = blueScene;
+              }
+              imgString = imgString.startsWith("https") ? imgString : "http://localhost:8080/logos/" + imgString;
+              if (time.act === "break") {
+                return;
+              } else {
+                return (
+                  <article key={index}>
+                    <section className="band">
+                      <img className="scene_img" src={sceneImgSrc} alt="scenes" />
+                      <h2>{time.act}</h2>
+                      <img className="band_img" src={imgString} alt="band image" />
+                    </section>
+                    <section className="time">
+                      <p>{time.start + "-" + time.end}</p>
+                    </section>
+                  </article>
+                );
+              }
+            })
+          : data.map((band, index) => (
+              <article key={index}>
+                <section className="band">
+                  <h2>{band.name}</h2>
+                  <img className="band_img" src={band.logo.startsWith("https") ? band.logo : "http://localhost:8080/logos/" + band.logo} alt="band image" />
+                </section>
+              </article>
+            ))}
       </section>
     </main>
   );
