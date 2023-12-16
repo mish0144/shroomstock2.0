55a60d98cf0bb9d82ac64cb7b137e882dfc19949
Misha Danneværn Marker <mish0144@stud.kea.dk>
2023-12-16 09:23:15 +0100

    nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,12 @@
 import "../css/style.css";
 import logo from "../img/logo.png";
+import { Link } from "react-router-dom";
 
 function Index() {
   return (
     <>
       <nav>
-        <img className="nav_logo" src={logo} alt="logo" />
+        <Link to="/"><img className="nav_logo" src={logo} alt="logo" /></Link>
         <h2>MENU</h2>
       </nav>
     </>
